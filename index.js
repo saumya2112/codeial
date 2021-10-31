0f026e09a55559029c011e3d88f8931877d8dc25
saumya <shreesaumya21@gmail.com>
2021-10-31 10:56:43 +0530

    connected database

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const port = 8000;
 const app = express();
-
 const expressLayouts = require('express-ejs-layouts');
-app.use(expressLayouts);
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
+
+app.use(expressLayouts);
 
 //extract style and scripts from sub pages into the layout
 app.set('layout extractStyles' , true);
