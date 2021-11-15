f45da4769272735f4850d61e47484cc9c4383d16
saumya <shreesaumya21@gmail.com>
2021-11-15 16:03:56 +0530

    connected database and added routes and controolers for signUp and signIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,15 @@ module.exports.profile = function(req , res){
         title : 'users_profile'
     });
 }
+// render the signUp page
+ module.exports.signUp = function(req , res){
+    res.render('user_sign_up' , {
+        title: 'codeial | signUp'
+    });
+};
+// render the signIn page
+ module.exports.signIn = function(req , res){
+    res.render('user_sign_in' , {
+        title: 'codeial | signIn'
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 router.get('/profile' , usersController.profile);
+router.get('/sign-Up' , usersController.signUp);
+router.get('/sign-In' , usersController.signIn);
 router.use('/posts' , require('./posts'));
 
 module.exports = router;
